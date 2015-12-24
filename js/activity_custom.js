4deabe7786d7d18cac0212ea0bfd11e7c15203fa
Daksh Shah <dakshshah@live.com>
2015-12-24 13:13:02 +0530

    Porting to canvas - loading image

--- js/activity_custom.js
+++ js/activity_custom.js
@@ -5,8 +5,11 @@ var oWidth = 0, oHeight = 0;
 
 $(document).ready(function() {
     var image = $('#principal-image');
-    canvas = $('#myCanvas');
+    //canvas = $('#myCanvas');
+    var canvas = document.getElementById("myCanvas");
+    //var image_for_canvas = document.getElementById("principal-image");
     var choose = $('#init-image');
+    var cContext = canvas.getContext('2d');
 
     image.css('position', 'relative');
 
@@ -14,13 +17,18 @@ $(document).ready(function() {
 
     $("#zoom-in").click(function() {
         console.log("Zoom-In");
-        var resize = 140; // resize amount in percentage
+        /*var resize = 140; // resize amount in percentage
         var newH   = iHeight * (resize / 100);
         var newW   = iWidth * (resize / 100);
         image.css('height', newH);
         image.css('width', newW);
         iWidth = newW;
-        iHeight = newH;
+        iHeight = newH;*/
+
+        //cContext.scale(1.2,1.2);
+        //canvas.setAttribute('width', image[0].width*1.2);
+        //canvas.setAttribute('height', image[0].height*1.2);
+        cContext.drawImage(image[0], 0,0,image[0].width*1.2, image[0].height*1.2);
     });
 
     $("#zoom-out").click(function() {
@@ -63,24 +71,24 @@ $(document).ready(function() {
     });
 
     function rotateImage(degree) {
-      var cContext = canvas.getContext('2d');
-      var cw = image.width, ch = image.height, cx = 0, cy = 0;
+      //var cContext = canvas.getContext('2d'); //moved to global
+      var cw = image[0].width, ch = image[0].height, cx = 0, cy = 0;
 
       //   Calculate new canvas size and x/y coorditates for image
       switch(rot){
         case 90:
-          cw = image.height;
-          ch = image.width;
-          cy = image.height * (-1);
+          cw = image[0].height;
+          ch = image[0].width;
+          cy = image[0].height * (-1);
           break;
         case 180:
-          cx = image.width * (-1);
-          cy = image.height * (-1);
+          cx = image[0].width * (-1);
+          cy = image[0].height * (-1);
           break;
         case 270:
-          cw = image.height;
-          ch = image.width;
-          cx = image.width * (-1);
+          cw = image[0].height;
+          ch = image[0].width;
+          cx = image[0].width * (-1);
           break;
         }
 
@@ -88,7 +96,7 @@ $(document).ready(function() {
       canvas.setAttribute('width', cw);
       canvas.setAttribute('height', ch);
       cContext.rotate(degree * Math.PI / 180);
-      cContext.drawImage(image, cx, cy);
+      cContext.drawImage(image[0], cx, cy);
       }
 
     $("#fullscreen").click(function() {
@@ -116,18 +124,30 @@ $(document).ready(function() {
 
       fileInput.addEventListener('change', function(e) {
         var file = fileInput.files[0];
+
         if (file.type.match(imageType)) {
             var reader = new FileReader();
             reader.onload = function(e) {
             var imageSrc = reader.result;
+
+            var dimg = new Image;
+            dimg.src = imageSrc;
+
             choose.css('display', 'none');
-            image.attr("src", imageSrc);
-            image.css('display', '');
+            image.attr('src', imageSrc);
+            image.css('display', 'none');
+
+            canvas.setAttribute('width', image[0].width);
+            canvas.setAttribute('height', image[0].height);
+            cContext.drawImage(image[0], 0,0);
+            alert('the image is drawn');
+
+            //Setting heights and Widths in variables
             iWidth = image.width();
             iHeight = image.height();
             oWidth = image.width();
-            oHeight = image.height();
-            $('#myCanvas').remove();
+            oHeight = image.height(); 
+            //$('#myCanvas').remove();
           }
           reader.readAsDataURL(file);
         }
